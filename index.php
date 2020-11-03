3b211d5817c0ed20b32ef26b40894e8e483a115a
jatin chawda <38835306+jatinchawda1503@users.noreply.github.com>
2020-11-03 14:23:34 +0530

    setup first

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Sports Website</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+</head>
+
+
+<body>
+
+
+
+
+</body>
+
+</html>
