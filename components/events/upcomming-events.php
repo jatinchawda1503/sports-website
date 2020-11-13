97a740098f06200a8d8b7e977d35b652a4f774cf
jatin chawda <38835306+jatinchawda1503@users.noreply.github.com>
2020-11-13 23:08:38 +0530

    push

--- components/events/upcomming-events.php
+++ components/events/upcomming-events.php
@@ -1,11 +1,11 @@
 <section class="comming-events">
    
         <div class="row m-0  justify-content-center align-items-start">
-            <div class="col-xl-3 col-md-4 col-sm-12">
+            <div class="col-xl-4 col-md-4 col-sm-12">
                 <h3>Up Comming Events</h3>
             </div>
 
-            <div class="col-xl-9 col-md-8 col-sm-12">
+            <div class="col-xl-8 col-md-8 col-sm-12 p-0">
                 <div class="comming-inner">
                     <div class="comming-single" style="background-image: url('https://images.unsplash.com/photo-1593766832792-804353f52dbf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1140&q=60')">
                     
