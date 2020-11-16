d4e0b59f765482c278883cde65f44357997ebb66
jatin chawda <38835306+jatinchawda1503@users.noreply.github.com>
2020-11-16 19:48:23 +0530

    go

--- components/teams/team-main.php
+++ components/teams/team-main.php
@@ -12,14 +12,14 @@
 
             <div class="col-xl-4 col-md-4 col-sm-12">
             <div class="person-image">
-							<img src="https://picsum.photos/300" alt="">
+					<img src="https://picsum.photos/300" alt="">
 			</div>
             </div>
 
             
             <div class="col-xl-8 col-md-8 col-sm-12">
-
-
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
             </div>
 
 
