6a53537662ba17b99bcbfea64a7d4411445a6570
jatin chawda <38835306+jatinchawda1503@users.noreply.github.com>
2020-11-17 17:25:46 +0530

    donw

--- components/category/category-list.php
+++ components/category/category-list.php
@@ -3,7 +3,7 @@
 
         <!-- Category Start -->
 
-        <div class="col-xl-12 col-md-12 col-sm-12 p-0 bg-al my-5">
+        <div class="col-xl-12 col-md-12 col-sm-12 p-0 bg-al my-1">
             <div class="row m-0 justify-content-center align-items-end">
                 <div class="col-xl-2 col-md-2 col-sm-12 list-img p-0">
                     <img src="https://images.unsplash.com/photo-1560088939-a88259febdce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=200&h=200&q=60" alt=""> 
@@ -17,5 +17,59 @@
         </div>
 
         <!-- Category End -->
+
+        <!-- Category Start -->
+
+        <div class="col-xl-12 col-md-12 col-sm-12 p-0 bg-al my-1">
+            <div class="row m-0 justify-content-center align-items-end">
+                <div class="col-xl-2 col-md-2 col-sm-12 list-img p-0">
+                    <img src="https://images.unsplash.com/photo-1461896836934-ffe607ba8211?ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=200&q=60" alt=""> 
+                </div>
+                <div class="col-xl-10 col-md-10 col-sm-12 list-content py-1">
+                    <h3>Running</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <button class="btn s-btn">View Category</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Category End -->
+
+         <!-- Category Start -->
+
+         <div class="col-xl-12 col-md-12 col-sm-12 p-0 bg-al my-1">
+            <div class="row m-0 justify-content-center align-items-end">
+                <div class="col-xl-2 col-md-2 col-sm-12 list-img p-0">
+                <img src="https://images.unsplash.com/photo-1490383559880-5003a7baa963?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=200&h=200&q=60" alt="">
+                </div>
+                <div class="col-xl-10 col-md-10 col-sm-12 list-content py-1">
+                    <h3>Chess</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <button class="btn s-btn">View Category</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Category End -->
+
+         <!-- Category Start -->
+
+         <div class="col-xl-12 col-md-12 col-sm-12 p-0 bg-al my-1">
+            <div class="row m-0 justify-content-center align-items-end">
+                <div class="col-xl-2 col-md-2 col-sm-12 list-img p-0">
+                    <img src="https://images.unsplash.com/photo-1600679472829-3044539ce8ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=200&h=200&q=60" alt="">
+                </div>
+                <div class="col-xl-10 col-md-10 col-sm-12 list-content py-1">
+                    <h3>Football</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <button class="btn s-btn">View Category</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Category End -->
+
+
+
     </div>
 </div>
---
+++ components/category/list-card.php
@@ -0,0 +1,21 @@
+<div class="container list-card">
+    <div class="row">
+
+        <!-- Category Start -->
+
+        <div class="col-xl-12 col-md-12 col-sm-12 p-0 bg-al my-1">
+            <div class="row m-0 justify-content-center align-items-start">
+                <div class="col-xl-4 col-md-4 col-sm-12 list-img p-0" id="list-img">
+                    <img src="https://images.unsplash.com/photo-1560088939-a88259febdce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=200&h=200&q=60" alt=""> 
+                </div>
+                <div class="col-xl-8 col-md-8 col-sm-12 list-content py-1">
+                    <h3>Swimming</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <button class="btn s-btn">View Category</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Category End -->
+    </div>
+</div>
